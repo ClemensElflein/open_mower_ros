86763623275b49f41e8b21bfd5c12c115a47197d
Yuki Onishi <yuki.st16@gmail.com>
2019-11-25 20:17:50 +0900

    Format ported codes

--- vesc_hi/include/vesc_driver.h
+++ vesc_hi/include/vesc_driver.h
@@ -1,10 +1,10 @@
 /*********************************************************************
 * Copyright (c) 2019, SoftBank Corp.
 * All rights reserved.
-* 
+*
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions are met:
-* 
+*
 *     * Redistributions of source code must retain the above copyright
 *       notice, this list of conditions and the following disclaimer.
 *     * Redistributions in binary form must reproduce the above copyright
@@ -13,7 +13,7 @@
 *     * Neither the name of Softbank Corp. nor the names of its
 *       contributors may be used to endorse or promote products derived from
 *       this software without specific prior written permission.
-* 
+*
 * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -28,8 +28,8 @@
 *********************************************************************/
 
 /** NOTE *************************************************************
-* This program had been developed by Michael T. Boulet at MIT under 
-* the BSD 3-clause License until Dec. 2016. Since Nov. 2019, Softbank 
+* This program had been developed by Michael T. Boulet at MIT under
+* the BSD 3-clause License until Dec. 2016. Since Nov. 2019, Softbank
 * Corp. takes over development as new packages.
 *********************************************************************/
 
@@ -46,72 +46,64 @@
 #include "vesc_driver/vesc_interface.h"
 #include "vesc_driver/vesc_packet.h"
 
-namespace vesc_driver
-{
+namespace vesc_driver {
 
-class VescDriver
-{
+class VescDriver {
 public:
-
-  VescDriver(ros::NodeHandle nh,
-             ros::NodeHandle private_nh);
+    VescDriver(ros::NodeHandle nh, ros::NodeHandle private_nh);
 
 private:
-  // interface to the VESC
-  VescInterface vesc_;
-  void vescPacketCallback(const boost::shared_ptr<VescPacket const>& packet);
-  void vescErrorCallback(const std::string& error);
+    // interface to the VESC
+    VescInterface vesc_;
+    void vescPacketCallback(const boost::shared_ptr<VescPacket const>& packet);
+    void vescErrorCallback(const std::string& error);
 
-  // limits on VESC commands
-  struct CommandLimit
-  {
-    CommandLimit(const ros::NodeHandle& nh, const std::string& str,
-                 const boost::optional<double>& min_lower = boost::optional<double>(),
-                 const boost::optional<double>& max_upper = boost::optional<double>());
-    double clip(double value);
-    std::string name;
-    boost::optional<double> lower;
-    boost::optional<double> upper;
-  };
-  CommandLimit duty_cycle_limit_;
-  CommandLimit current_limit_;
-  CommandLimit brake_limit_;
-  CommandLimit speed_limit_;
-  CommandLimit position_limit_;
-  CommandLimit servo_limit_;
+    // limits on VESC commands
+    struct CommandLimit {
+        CommandLimit(const ros::NodeHandle& nh, const std::string& str,
+                     const boost::optional<double>& min_lower = boost::optional<double>(),
+                     const boost::optional<double>& max_upper = boost::optional<double>());
+        double clip(double value);
+        std::string             name;
+        boost::optional<double> lower;
+        boost::optional<double> upper;
+    };
+    CommandLimit duty_cycle_limit_;
+    CommandLimit current_limit_;
+    CommandLimit brake_limit_;
+    CommandLimit speed_limit_;
+    CommandLimit position_limit_;
+    CommandLimit servo_limit_;
 
-  // ROS services
-  ros::Publisher state_pub_;
-  ros::Publisher servo_sensor_pub_;
-  ros::Subscriber duty_cycle_sub_;
-  ros::Subscriber current_sub_;
-  ros::Subscriber brake_sub_;
-  ros::Subscriber speed_sub_;
-  ros::Subscriber position_sub_;
-  ros::Subscriber servo_sub_;
-  ros::Timer timer_;
+    // ROS services
+    ros::Publisher  state_pub_;
+    ros::Publisher  servo_sensor_pub_;
+    ros::Subscriber duty_cycle_sub_;
+    ros::Subscriber current_sub_;
+    ros::Subscriber brake_sub_;
+    ros::Subscriber speed_sub_;
+    ros::Subscriber position_sub_;
+    ros::Subscriber servo_sub_;
+    ros::Timer      timer_;
 
-  // driver modes (possible states)
-  typedef enum {
-    MODE_INITIALIZING,
-    MODE_OPERATING
-  } driver_mode_t;
+    // driver modes (possible states)
+    typedef enum { MODE_INITIALIZING, MODE_OPERATING } driver_mode_t;
 
-  // other variables
-  driver_mode_t driver_mode_;           ///< driver state machine mode (state)
-  int fw_version_major_;                ///< firmware major version reported by vesc
-  int fw_version_minor_;                ///< firmware minor version reported by vesc
+    // other variables
+    driver_mode_t driver_mode_;       ///< driver state machine mode (state)
+    int           fw_version_major_;  ///< firmware major version reported by vesc
+    int           fw_version_minor_;  ///< firmware minor version reported by vesc
 
-  // ROS callbacks
-  void timerCallback(const ros::TimerEvent& event);
-  void dutyCycleCallback(const std_msgs::Float64::ConstPtr& duty_cycle);
-  void currentCallback(const std_msgs::Float64::ConstPtr& current);
-  void brakeCallback(const std_msgs::Float64::ConstPtr& brake);
-  void speedCallback(const std_msgs::Float64::ConstPtr& speed);
-  void positionCallback(const std_msgs::Float64::ConstPtr& position);
-  void servoCallback(const std_msgs::Float64::ConstPtr& servo);
+    // ROS callbacks
+    void timerCallback(const ros::TimerEvent& event);
+    void dutyCycleCallback(const std_msgs::Float64::ConstPtr& duty_cycle);
+    void currentCallback(const std_msgs::Float64::ConstPtr& current);
+    void brakeCallback(const std_msgs::Float64::ConstPtr& brake);
+    void speedCallback(const std_msgs::Float64::ConstPtr& speed);
+    void positionCallback(const std_msgs::Float64::ConstPtr& position);
+    void servoCallback(const std_msgs::Float64::ConstPtr& servo);
 };
 
-} // namespace vesc_driver
+}  // namespace vesc_driver
 
-#endif // VESC_DRIVER_VESC_DRIVER_H_
+#endif  // VESC_DRIVER_VESC_DRIVER_H_
--- vesc_hi/include/vesc_packet.h
+++ vesc_hi/include/vesc_packet.h
@@ -1,10 +1,10 @@
 /*********************************************************************
 * Copyright (c) 2019, SoftBank Corp.
 * All rights reserved.
-* 
+*
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions are met:
-* 
+*
 *     * Redistributions of source code must retain the above copyright
 *       notice, this list of conditions and the following disclaimer.
 *     * Redistributions in binary form must reproduce the above copyright
@@ -13,7 +13,7 @@
 *     * Neither the name of Softbank Corp. nor the names of its
 *       contributors may be used to endorse or promote products derived from
 *       this software without specific prior written permission.
-* 
+*
 * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -28,8 +28,8 @@
 *********************************************************************/
 
 /** NOTE *************************************************************
-* This program had been developed by Michael T. Boulet at MIT under 
-* the BSD 3-clause License until Dec. 2016. Since Nov. 2019, Softbank 
+* This program had been developed by Michael T. Boulet at MIT under
+* the BSD 3-clause License until Dec. 2016. Since Nov. 2019, Softbank
 * Corp. takes over development as new packages.
 *********************************************************************/
 
@@ -44,183 +44,173 @@
 #include <boost/crc.hpp>
 #include <boost/shared_ptr.hpp>
 
-namespace vesc_driver
-{
+namespace vesc_driver {
 
 typedef std::vector<uint8_t> Buffer;
-typedef std::pair<Buffer::iterator, Buffer::iterator> BufferRange;
+typedef std::pair<Buffer::iterator, Buffer::iterator>             BufferRange;
 typedef std::pair<Buffer::const_iterator, Buffer::const_iterator> BufferRangeConst;
 
 /** The raw frame for communicating with the VESC */
-class VescFrame
-{
+class VescFrame {
 public:
-  virtual ~VescFrame() {}
+    virtual ~VescFrame() {
+    }
 
-  // getters
-  virtual const Buffer& frame() const {return *frame_;}
+    // getters
+    virtual const Buffer& frame() const {
+        return *frame_;
+    }
 
-  // VESC packet properties
-  static const int VESC_MAX_PAYLOAD_SIZE = 1024;          ///< Maximum VESC payload size, in bytes
-  static const int VESC_MIN_FRAME_SIZE = 5;               ///< Smallest VESC frame size, in bytes
-  static const int VESC_MAX_FRAME_SIZE = 6 + VESC_MAX_PAYLOAD_SIZE; ///< Largest VESC frame size, in bytes
-  static const unsigned int VESC_SOF_VAL_SMALL_FRAME = 2; ///< VESC start of "small" frame value
-  static const unsigned int VESC_SOF_VAL_LARGE_FRAME = 3; ///< VESC start of "large" frame value
-  static const unsigned int VESC_EOF_VAL = 3;             ///< VESC end-of-frame value
+    // VESC packet properties
+    static const int          VESC_MAX_PAYLOAD_SIZE = 1024;  ///< Maximum VESC payload size, in bytes
+    static const int          VESC_MIN_FRAME_SIZE   = 5;     ///< Smallest VESC frame size, in bytes
+    static const int          VESC_MAX_FRAME_SIZE   = 6 + VESC_MAX_PAYLOAD_SIZE;  ///< Largest VESC frame size, in bytes
+    static const unsigned int VESC_SOF_VAL_SMALL_FRAME = 2;                       ///< VESC start of "small" frame value
+    static const unsigned int VESC_SOF_VAL_LARGE_FRAME = 3;                       ///< VESC start of "large" frame value
+    static const unsigned int VESC_EOF_VAL             = 3;                       ///< VESC end-of-frame value
 
-  /** CRC parameters for the VESC */
-  typedef boost::crc_optimal<16, 0x1021, 0, 0, false, false> CRC;
+    /** CRC parameters for the VESC */
+    typedef boost::crc_optimal<16, 0x1021, 0, 0, false, false> CRC;
 
 protected:
-  /** Construct frame with specified payload size. */
-  VescFrame(int payload_size);
+    /** Construct frame with specified payload size. */
+    VescFrame(int payload_size);
 
-  boost::shared_ptr<Buffer> frame_; ///< Stores frame data, shared_ptr for shallow copy
-  BufferRange payload_;             ///< View into frame's payload section
+    boost::shared_ptr<Buffer> frame_;    ///< Stores frame data, shared_ptr for shallow copy
+    BufferRange               payload_;  ///< View into frame's payload section
 
 private:
-  /** Construct from buffer. Used by VescPacketFactory factory. */
-  VescFrame(const BufferRangeConst& frame, const BufferRangeConst& payload);
+    /** Construct from buffer. Used by VescPacketFactory factory. */
+    VescFrame(const BufferRangeConst& frame, const BufferRangeConst& payload);
 
-  /** Give VescPacketFactory access to private constructor. */
-  friend class VescPacketFactory;
+    /** Give VescPacketFactory access to private constructor. */
+    friend class VescPacketFactory;
 };
 
 /*------------------------------------------------------------------------------------------------*/
 
 /** A VescPacket is a VescFrame with a non-zero length payload */
-class VescPacket : public VescFrame
-{
+class VescPacket : public VescFrame {
 public:
-  virtual ~VescPacket() {}
+    virtual ~VescPacket() {
+    }
 
-  virtual const std::string& name() const {return name_;}
+    virtual const std::string& name() const {
+        return name_;
+    }
 
 protected:
-  VescPacket(const std::string& name, int payload_size, int payload_id);
-  VescPacket(const std::string& name, boost::shared_ptr<VescFrame> raw);
+    VescPacket(const std::string& name, int payload_size, int payload_id);
+    VescPacket(const std::string& name, boost::shared_ptr<VescFrame> raw);
 
 private:
-  std::string name_;
+    std::string name_;
 };
 
-typedef boost::shared_ptr<VescPacket> VescPacketPtr;
+typedef boost::shared_ptr<VescPacket>       VescPacketPtr;
 typedef boost::shared_ptr<VescPacket const> VescPacketConstPtr;
 
 /*------------------------------------------------------------------------------------------------*/
 
-class VescPacketFWVersion : public VescPacket
-{
+class VescPacketFWVersion : public VescPacket {
 public:
-  VescPacketFWVersion(boost::shared_ptr<VescFrame> raw);
+    VescPacketFWVersion(boost::shared_ptr<VescFrame> raw);
 
-  int fwMajor() const;
-  int fwMinor() const;
-
+    int fwMajor() const;
+    int fwMinor() const;
 };
 
-class VescPacketRequestFWVersion : public VescPacket
-{
+class VescPacketRequestFWVersion : public VescPacket {
 public:
-  VescPacketRequestFWVersion();
-
+    VescPacketRequestFWVersion();
 };
 
 /*------------------------------------------------------------------------------------------------*/
 
-class VescPacketValues : public VescPacket
-{
+class VescPacketValues : public VescPacket {
 public:
-  VescPacketValues(boost::shared_ptr<VescFrame> raw);
+    VescPacketValues(boost::shared_ptr<VescFrame> raw);
 
-  double v_in() const;
-  double temp_mos1() const;
-  double temp_mos2() const;
-  double temp_mos3() const;
-  double temp_mos4() const;
-  double temp_mos5() const;
-  double temp_mos6() const;
-  double temp_pcb() const;
-  double current_motor() const;
-  double current_in() const;
-  double rpm() const;
-  double duty_now() const;
-  double amp_hours() const;
-  double amp_hours_charged() const;
-  double watt_hours() const;
-  double watt_hours_charged() const;
-  double tachometer() const;
-  double tachometer_abs() const;
-  int fault_code() const;
-
+    double v_in() const;
+    double temp_mos1() const;
+    double temp_mos2() const;
+    double temp_mos3() const;
+    double temp_mos4() const;
+    double temp_mos5() const;
+    double temp_mos6() const;
+    double temp_pcb() const;
+    double current_motor() const;
+    double current_in() const;
+    double rpm() const;
+    double duty_now() const;
+    double amp_hours() const;
+    double amp_hours_charged() const;
+    double watt_hours() const;
+    double watt_hours_charged() const;
+    double tachometer() const;
+    double tachometer_abs() const;
+    int    fault_code() const;
 };
 
-class VescPacketRequestValues : public VescPacket
-{
+class VescPacketRequestValues : public VescPacket {
 public:
-  VescPacketRequestValues();
+    VescPacketRequestValues();
 };
 
 /*------------------------------------------------------------------------------------------------*/
 
-class VescPacketSetDuty : public VescPacket
-{
+class VescPacketSetDuty : public VescPacket {
 public:
-  VescPacketSetDuty(double duty);
+    VescPacketSetDuty(double duty);
 
-  //  double duty() const;
+    //  double duty() const;
 };
 
 /*------------------------------------------------------------------------------------------------*/
 
-class VescPacketSetCurrent : public VescPacket
-{
+class VescPacketSetCurrent : public VescPacket {
 public:
-  VescPacketSetCurrent(double current);
+    VescPacketSetCurrent(double current);
 
-  //  double current() const;
+    //  double current() const;
 };
 
 /*------------------------------------------------------------------------------------------------*/
 
-class VescPacketSetCurrentBrake : public VescPacket
-{
+class VescPacketSetCurrentBrake : public VescPacket {
 public:
-  VescPacketSetCurrentBrake(double current_brake);
+    VescPacketSetCurrentBrake(double current_brake);
 
-  //  double current_brake() const;
+    //  double current_brake() const;
 };
 
 /*------------------------------------------------------------------------------------------------*/
 
-class VescPacketSetRPM : public VescPacket
-{
+class VescPacketSetRPM : public VescPacket {
 public:
-  VescPacketSetRPM(double rpm);
+    VescPacketSetRPM(double rpm);
 
-  //  double rpm() const;
+    //  double rpm() const;
 };
 
 /*------------------------------------------------------------------------------------------------*/
 
-class VescPacketSetPos : public VescPacket
-{
+class VescPacketSetPos : public VescPacket {
 public:
-  VescPacketSetPos(double pos);
+    VescPacketSetPos(double pos);
 
-  //  double pos() const;
+    //  double pos() const;
 };
 
 /*------------------------------------------------------------------------------------------------*/
 
-class VescPacketSetServoPos : public VescPacket
-{
+class VescPacketSetServoPos : public VescPacket {
 public:
-  VescPacketSetServoPos(double servo_pos);
+    VescPacketSetServoPos(double servo_pos);
 
-  //  double servo_pos() const;
+    //  double servo_pos() const;
 };
 
-} // namespace vesc_driver
+}  // namespace vesc_driver
 
-#endif // VESC_DRIVER_VESC_PACKET_H_
+#endif  // VESC_DRIVER_VESC_PACKET_H_
