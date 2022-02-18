a1f5ae27e641960dcc3564167e4069f45bebf8de
Clemens Elflein <clemens1@familie-elflein.de>
2022-02-18 17:39:08 +0100

    added copyright header

--- mower_simulation/src/mower_simulation.cpp
+++ mower_simulation/src/mower_simulation.cpp
@@ -1,3 +1,19 @@
+// Created by Clemens Elflein on 2/18/22, 5:37 PM.
+// Copyright (c) 2022 Clemens Elflein. All rights reserved.
+//
+// This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+//
+// Feel free to use the design in your private/educational projects, but don't try to sell the design or products based on it without getting my consent first.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+//
 #include "ros/ros.h"
 
 // Include messages for mower control
