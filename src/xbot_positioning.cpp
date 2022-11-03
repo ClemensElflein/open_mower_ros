68e59c29f80feff56b7c13e3170f3e4018c6abd3
Clemens Elflein <clemens1@familie-elflein.de>
2022-11-03 02:27:38 +0100

    feat: debug messages can be disabled, some refactoring

--- src/xbot_positioning.cpp
+++ src/xbot_positioning.cpp
@@ -20,28 +20,40 @@
 #include "xbot_msgs/WheelTick.h"
 #include "xbot_positioning/KalmanState.h"
 
-ros::Publisher odometry_pub_predict_only;
 ros::Publisher odometry_pub;
-ros::Publisher dbg_expected_motion_vector;
+
+// Debug Publishers
 ros::Publisher kalman_state;
+ros::Publisher dbg_expected_motion_vector;
 
+// The kalman filters
 xbot::positioning::xbot_positioning_core core;
-xbot::positioning::xbot_positioning_core predict_only_core;
 
+// True, if we don't want to do gyro calibration on launch
 bool skip_gyro_calibration;
+
+// True, if we have wheel ticks (i.e. last_ticks is valid)
 bool has_ticks;
 xbot_msgs::WheelTick last_ticks;
 
-sensor_msgs::Imu last_imu;
+
+// True, if last_imu is valid and gyro_offset is valid
 bool has_gyro;
+sensor_msgs::Imu last_imu;
 ros::Time gyro_calibration_start;
 double gyro_offset;
 int gyro_offset_samples;
+
+// Current speed calculated by wheel ticks
 double vx = 0.0;
+
+// Min speed for motion vector to be fed into kalman filter
 double min_speed = 0.0;
 
-nav_msgs::Odometry odometry, odometry_predict_only;
+// True, if we should publish debug topics (expected motion vector and kalman state)
+bool publish_debug;
 
+nav_msgs::Odometry odometry;
 xbot_positioning::KalmanState state_msg;
 
 void onImu(const sensor_msgs::Imu::ConstPtr &msg) {
@@ -75,8 +87,6 @@ void onImu(const sensor_msgs::Imu::ConstPtr &msg) {
 
     core.predict(vx, -msg->angular_velocity.z - gyro_offset, (msg->header.stamp - last_imu.header.stamp).toSec());
     auto x = core.updateSpeed(vx, -msg->angular_velocity.z - gyro_offset,0.01);
-    predict_only_core.predict(vx, -msg->angular_velocity.z - gyro_offset, (msg->header.stamp - last_imu.header.stamp).toSec());
-    auto x_predict_only = predict_only_core.updateSpeed(vx, -msg->angular_velocity.z - gyro_offset,0.01);
 
     odometry.header.stamp = ros::Time::now();
     odometry.header.seq++;
@@ -87,26 +97,19 @@ void onImu(const sensor_msgs::Imu::ConstPtr &msg) {
     tf2::Quaternion q(0.0, 0.0, x.theta());
     odometry.pose.pose.orientation = tf2::toMsg(q);
 
-    odometry_predict_only.header = odometry.header;
-    odometry_predict_only.child_frame_id = odometry.child_frame_id;
-    odometry_predict_only.pose.pose.position.x = x_predict_only.x_pos();
-    odometry_predict_only.pose.pose.position.y = x_predict_only.y_pos();
-    tf2::Quaternion q_predict_only(0.0, 0.0, x_predict_only.theta());
-    odometry_predict_only.pose.pose.orientation = tf2::toMsg(q_predict_only);
 
-    ROS_INFO_STREAM_THROTTLE(1,"c:\n" << core.getCovariance() << "\n\n" << "state\n:" << core.getState());
+    if(publish_debug) {
+        auto state = core.getState();
+        state_msg.x = state.x();
+        state_msg.y = state.y();
+        state_msg.theta = state.theta();
+        state_msg.vx = state.vx();
+        state_msg.vr = state.vr();
 
-    auto state = core.getState();
-    state_msg.x = state.x();
-    state_msg.y = state.y();
-    state_msg.theta = state.theta();
-    state_msg.vx = state.vx();
-    state_msg.vr = state.vr();
-
-    kalman_state.publish(state_msg);
+        kalman_state.publish(state_msg);
+    }
 
     odometry_pub.publish(odometry);
-    odometry_pub_predict_only.publish(odometry_predict_only);
 
     last_imu = *msg;
 }
@@ -139,15 +142,14 @@ void onWheelTicks(const xbot_msgs::WheelTick::ConstPtr &msg) {
 void onPose(const xbot_msgs::AbsolutePose::ConstPtr &msg) {
 
     core.updatePosition(msg->pose.pose.position.x, msg->pose.pose.position.y);
-//    double c = 100000.0*pow(last_imu.angular_velocity.z - gyro_offset,2) + 10000.0;
-//    core.updateOrientation(msg->motion_heading, 0.001);
-auto m = core.om2.h(core.ekf.getState());
-geometry_msgs::Vector3 dbg;
-dbg.x = m.vx();
-dbg.y = m.vy();
-    dbg_expected_motion_vector.publish(dbg);
-
-    if(std::sqrt(std::pow(msg->motion_vector.x, 2)+std::pow(msg->motion_vector.y, 2)) > min_speed) {
+    if(publish_debug) {
+        auto m = core.om2.h(core.ekf.getState());
+        geometry_msgs::Vector3 dbg;
+        dbg.x = m.vx();
+        dbg.y = m.vy();
+        dbg_expected_motion_vector.publish(dbg);
+    }
+    if(std::sqrt(std::pow(msg->motion_vector.x, 2)+std::pow(msg->motion_vector.y, 2)) >= min_speed) {
         core.updateOrientation2(msg->motion_vector.x, msg->motion_vector.y, 10000.0);
     }
 }
@@ -167,15 +169,17 @@ int main(int argc, char **argv) {
     paramNh.param("skip_gyro_calibration", skip_gyro_calibration, false);
     paramNh.param("gyro_offset", gyro_offset, 0.0);
     paramNh.param("min_speed", min_speed, 0.01);
+    paramNh.param("debug", publish_debug, false);
 
-    if(gyro_offset != 0.0) {
+    if(gyro_offset != 0.0 && skip_gyro_calibration) {
         ROS_WARN_STREAM("Using gyro offset of: " << gyro_offset);
     }
 
     odometry_pub = paramNh.advertise<nav_msgs::Odometry>("odom", 50);
-    odometry_pub_predict_only = paramNh.advertise<nav_msgs::Odometry>("odom_predict_only", 50);
-    dbg_expected_motion_vector = paramNh.advertise<geometry_msgs::Vector3>("debug_expected_motion_vector", 50);
-    kalman_state = paramNh.advertise<xbot_positioning::KalmanState>("kalman_state", 50);
+    if(publish_debug) {
+        dbg_expected_motion_vector = paramNh.advertise<geometry_msgs::Vector3>("debug_expected_motion_vector", 50);
+        kalman_state = paramNh.advertise<xbot_positioning::KalmanState>("kalman_state", 50);
+    }
 
     ros::Subscriber imu_sub = paramNh.subscribe("imu", 10, onImu);
     ros::Subscriber pose_sub = paramNh.subscribe("xb_pose", 10, onPose);
