4dc82fac0ebff8ade2e27460ac0c314151f9e28a
Clemens Elflein <clemens1@familie-elflein.de>
2024-05-25 13:58:11 +0200

    Generate version info from git tag, write to environment file and pass it to monitoring node.

--- .github/assets/openmower_entrypoint.sh
+++ .github/assets/openmower_entrypoint.sh
@@ -9,4 +9,6 @@ source /config/mower_config.sh
 # source the hardware specific default environment (default wheel ticks, antenna position etc)
 source $(rospack find open_mower)/params/hardware_specific/$OM_MOWER/default_environment.sh
 
+source /opt/open_mower_ros/version_info.env
+
 exec "$@"
