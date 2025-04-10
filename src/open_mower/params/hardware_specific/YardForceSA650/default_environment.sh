9f65b1206e7735f863bf6913fabd1883259caa18
Oliver Walsh <ollie.walsh@gmail.com>
2025-04-10 18:34:40 +0100

    Handle uart mapping changing on newer kernel (#175)
    Since 6.1.28 the uart mappings have changed:
    https://forums.raspberrypi.com/viewtopic.php?t=347868#p2106791

--- src/open_mower/params/hardware_specific/YardForceSA650/default_environment.sh
+++ src/open_mower/params/hardware_specific/YardForceSA650/default_environment.sh
@@ -7,3 +7,16 @@ export OM_WHEEL_DISTANCE_M=${WHEEL_DISTANCE_M:-0.325}
 
 # Set default ticks/m
 export OM_WHEEL_TICKS_PER_M=${OM_WHEEL_TICKS_PER_M:-1964.0}
+
+# Serial ports change when kernel version >= 6.1.28
+# https://forums.raspberrypi.com/viewtopic.php?t=347868#p2106791
+KERNEL_VERSION_MAJOR=$(uname -r | cut -d. -f1)
+KERNEL_VERSION_MINOR=$(uname -r | cut -d. -f2)
+KERNEL_VERSION_PATCH=$(uname -r | cut -d. -f3 | sed -e 's/^\([0-9]\+\).*/\1/')
+KERNEL_VERSION=$((KERNEL_VERSION_MAJOR * 10000 + KERNEL_VERSION_MINOR * 100 + KERNEL_VERSION_PATCH))
+if [ $KERNEL_VERSION -lt 60128 ]; then
+    export OM_XESC_LEFT_PORT=${OM_XESC_LEFT_PORT:-/dev/ttyAMA4}
+    export OM_XESC_RIGHT_PORT=${OM_XESC_RIGHT_PORT:-/dev/ttyAMA2}
+    export OM_XESC_MOWER_PORT=${OM_XESC_MOWER_PORT:-/dev/ttyAMA3}
+    export OM_GPS_PORT=${OM_GPS_PORT:-/dev/ttyAMA1}
+fi
