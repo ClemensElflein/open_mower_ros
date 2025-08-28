d6d803e43f3b2b96e2e45e82e87a88d287373c56
Robert Vollmer <dev@robv.de>
2025-08-28 19:50:10 +0000

    Include datum in MQTT map payload

--- src/lib/xbot_monitoring/src/xbot_monitoring.cpp
+++ src/lib/xbot_monitoring/src/xbot_monitoring.cpp
@@ -48,6 +48,10 @@ std::mutex mqtt_callback_mutex;
 ros::Publisher cmd_vel_pub;
 ros::Publisher action_pub;
 
+// Datum
+double datum_lat, datum_long, datum_height;
+bool has_datum = false;
+
 // properties for external mqtt
 bool external_mqtt_enable = false;
 std::string external_mqtt_username = "";
@@ -428,6 +432,12 @@ void map_callback(const xbot_msgs::Map::ConstPtr &msg) {
     // Build a JSON and publish it
     json j;
 
+    if (has_datum) {
+        j["datum"]["lat"] = datum_lat;
+        j["datum"]["long"] = datum_long;
+        j["datum"]["height"] = datum_height;
+    }
+
     j["docking_pose"]["x"] = msg->dockX;
     j["docking_pose"]["y"] = msg->dockY;
     j["docking_pose"]["heading"] = msg->dockHeading;
@@ -563,6 +573,11 @@ int main(int argc, char **argv) {
     if(version_string.empty()) {
         version_string = "UNKNOWN VERSION";
     }
+
+    has_datum = true;
+    has_datum &= n->getParam("/ll/services/gps/datum_lat", datum_lat);
+    has_datum &= n->getParam("/ll/services/gps/datum_long", datum_long);
+    has_datum &= n->getParam("/ll/services/gps/datum_height", datum_height);
 
     external_mqtt_enable = paramNh.param("external_mqtt_enable", false);
     external_mqtt_topic_prefix = paramNh.param("external_mqtt_topic_prefix", std::string(""));
