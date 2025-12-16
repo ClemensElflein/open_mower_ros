01376e614be215ffb9e21e31f052ea15d4da35d1
Jörg Ebeling <joerg@ebeling.ws>
2025-12-16 18:08:14 +0100

    🐛 fix(docker): ensure real-time logging order

--- docker/openmower_entrypoint.sh
+++ docker/openmower_entrypoint.sh
@@ -6,6 +6,10 @@ source "/opt/ros/$ROS_DISTRO/setup.bash"
 source /opt/open_mower_ros/devel/setup.bash
 source /opt/open_mower_ros/version_info.env
 
+# Ensure stdout and stderr are unbuffered to get logging in real time order
+export ROSCONSOLE_STDOUT_LINE_BUFFERED=1
+export PYTHONUNBUFFERED=1
+
 # OSv2 debugging get controlled via env var DEBUG and has the ROSCONSOLE_CONFIG_FILE embedded
 shopt -s nocasematch
 case "${DEBUG:-0}" in
@@ -20,4 +24,4 @@ case "${DEBUG:-0}" in
 esac
 shopt -u nocasematch || true
 
-exec -- "$@"
+exec stdbuf -oL -eL -- "$@"
