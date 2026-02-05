c2cf073fd10a52403072ab83b304292d886650a9
Jörg Ebeling <joerg@ebeling.ws>
2026-02-05 18:39:24 +0100

    ♻️ refactor(docker): (ROSCONSOLE_STDOUT_LINE_BUFFERED, PYTHONUNBUFFERED) to a consolidated location before exec - apply consistent changes to both legacy and standard entrypoint scripts

--- docker/openmower_entrypoint.legacy.sh
+++ docker/openmower_entrypoint.legacy.sh
@@ -5,11 +5,10 @@ set -e
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 source /opt/open_mower_ros/devel/setup.bash
 
-# Ensure stdout and stderr are unbuffered to get logging in real time order
-export ROSCONSOLE_STDOUT_LINE_BUFFERED=1
-export PYTHONUNBUFFERED=1
-
+# setup om environment
+source /opt/open_mower_ros/version_info.env
 source /config/mower_config.sh
+
 # If OM_V2 is truthy, set HARDWARE_PLATFORM=2 and new (yaml-based) config, else 1 and environment config
 if [[ "${OM_V2,,}" =~ ^(true|1|yes)$ ]]; then
     export HARDWARE_PLATFORM=2
@@ -26,6 +25,8 @@ source "$(rospack find open_mower)/params/hardware_specific/$MOWER/default_envir
 export RECORDINGS_PATH=$HOME
 export PARAMS_PATH=$HOME
 
-source /opt/open_mower_ros/version_info.env
+# Ensure stdout and stderr are unbuffered to get logging in real time order
+export ROSCONSOLE_STDOUT_LINE_BUFFERED=1
+export PYTHONUNBUFFERED=1
 
-exec stdbuf -oL -eL -- "$@"
+exec -- "$@"
